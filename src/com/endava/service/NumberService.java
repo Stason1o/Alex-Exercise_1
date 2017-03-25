a011d4fbb93703f7983feebf9a977e84ecfa58ad
Stason1o <stasikus1996@gmail.com>
2017-03-26 00:39:14 +0200

    added printDate method which prints current date. added System out println in each method, which prints info at the beginning and ending of method

--- src/com/endava/service/NumberService.java
+++ src/com/endava/service/NumberService.java
@@ -5,6 +5,9 @@ import com.endava.entity.MobilePhone;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Stas on 22.03.2017.
@@ -29,6 +32,8 @@ public class NumberService {
     }
 
     private String validateNumber(){
+        System.out.println("NumberService.validateNumber: method will validate input data");
+        printDate();
         int amountOfInputAttempts = 0;
         String telephoneNumber;
         System.out.println("Enter your phone number: ");
@@ -37,12 +42,19 @@ public class NumberService {
 
             if(telephoneNumber.matches("[+][(][3][7][3][)]-[67][089]-[0-9]{2}-[0-9]{2}-[0-9]{2}")) {
                 this.setPhoneNumber(telephoneNumber);
+                System.out.println("NumberService.validateNumber: method returns input phone number as a string");
+                printDate();
                 return telephoneNumber;
+
             }else if (telephoneNumber.matches("[+][3][7][3] [67][089] [0-9]{6}")){
                 this.setPhoneNumber(telephoneNumber);
+                System.out.println("NumberService.validateNumber: method returns input phone number as a string");
+                printDate();
                 return telephoneNumber;
             }else if(telephoneNumber.matches("[+][3][7][3] [67][089] [0-9]{3}[-][0-9]{3}")){
                 this.setPhoneNumber(telephoneNumber);
+                System.out.println("NumberService.validateNumber: method returns input phone number as a string");
+                printDate();
                 return telephoneNumber;
             }
             else {
@@ -50,15 +62,18 @@ public class NumberService {
                 System.out.println("Possible types of number:");
                 System.out.println("+373 60 066006 / +(373)-79-85-85-85 / +373 79 666-999");
                 System.out.println("Remained attempts: " + (4 - amountOfInputAttempts));
+                printDate();
             }
         }while(amountOfInputAttempts < 4);
 
-        System.out.println("Input is wrong =( See you later =)");
+        System.out.println("NumberService.validateNumber: method validated input data and it's wrong =( See you later =)");
         this.setPhoneNumber("");
         return "";
     }
 
     private int checkMoneyAmount(){
+        System.out.println("NumberService.checkMoneyAmount: method will check input money");
+        printDate();
         if(getPhoneNumber().equals(""))
             return -1;
         int amountOfAttempts = 0;
@@ -77,6 +92,8 @@ public class NumberService {
             }
 
             if(amountOfMoney >= 50 && amountOfMoney <= 500){
+                System.out.println("NumberService.checkMoneyAmount: Input is correct. Proceeding....");
+                printDate();
                 return amountOfMoney;
             } else {
                 amountOfAttempts++;
@@ -85,7 +102,9 @@ public class NumberService {
                 regexValidator = true;
             }
         }
-        System.out.println("Wrong input. Sorry. Goodbye");
+
+        System.out.println("NumberService.checkMoneyAmount: Wrong input. Sorry. Goodbye");
+        printDate();
         return -1;
     }
 
@@ -94,6 +113,7 @@ public class NumberService {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             inputFromConsole = br.readLine();
+            printDate();
         }catch (IOException ex){
             System.err.println("Input/Output Exception!");
             ex.printStackTrace();
@@ -102,7 +122,17 @@ public class NumberService {
     }
 
     private boolean checkInputMoney(String string){
+        System.out.println("NumberService.checkInputMoney: method checks if input matches regex");
+        printDate();
         boolean checkIfMatches = (string.matches("[0-9]+"));
+        System.out.println("NumberService.checkInputMoney: method returns true/false ");
+        printDate();
         return checkIfMatches;
+    }
+
+    private void printDate(){
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        System.out.println("Action taken on: " + dateFormat.format(date));
     }
 }
